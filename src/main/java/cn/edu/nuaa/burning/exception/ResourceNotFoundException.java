401f98e881b5e7217eef6113b86e12a3891ac437
zxh <1158326098@qq.com>
2016-05-22 15:48:00 +0800

    更改登录时没找到用户的提示语

--- src/main/java/cn/edu/nuaa/burning/exception/ResourceNotFoundException.java
+++ src/main/java/cn/edu/nuaa/burning/exception/ResourceNotFoundException.java
@@ -8,6 +8,6 @@ public class ResourceNotFoundException extends BaseException {
     private static final long serialVersionUID = -7495933202664764663L;
 
     public ResourceNotFoundException() {
-        super(103, "指定的资源不存在。", 404);
+        super(103, "系统找不到指定的资源。", 404);
     }
 }
